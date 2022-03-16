25d1a0b1485fcf58743100369fd2b493c8c426eb
Eddie <eulerissmart@gmail.com>
2022-03-15 23:13:12 -0400

    added linting

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/App.scss";
-import React from "react";
-import debounce from "lodash.debounce";
-import MainSection from "./MainSection.js";
-import ResponsiveNav from "./ResponsiveNav.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/App.scss';
+import React from 'react';
+import debounce from 'lodash.debounce';
+import MainSection from './MainSection.js';
+import ResponsiveNav from './ResponsiveNav.js';
 
-const WelcomeSection = React.lazy(() => import("./WelcomeSection.js"));
-const MainMenuSection = React.lazy(() => import("./MainMenuSection.js"));
-const DessertSection = React.lazy(() => import("./DessertSection.js"));
-const DrinkSection = React.lazy(() => import("./DrinkSection.js"));
-const Reservation = React.lazy(() => import("./Reservation.js"));
-const Footer = React.lazy(() => import("./Footer.js"));
-const OrderSection = React.lazy(() => import("./OrderSection.js"));
-const Section1Hero = React.lazy(() => import("./Section1Hero.js"));
+const WelcomeSection = React.lazy(() => import('./WelcomeSection.js'));
+const MainMenuSection = React.lazy(() => import('./MainMenuSection.js'));
+const DessertSection = React.lazy(() => import('./DessertSection.js'));
+const DrinkSection = React.lazy(() => import('./DrinkSection.js'));
+const Reservation = React.lazy(() => import('./Reservation.js'));
+const Footer = React.lazy(() => import('./Footer.js'));
+const OrderSection = React.lazy(() => import('./OrderSection.js'));
+const Section1Hero = React.lazy(() => import('./Section1Hero.js'));
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     window.onscroll = debounce(() => {
-      var elementTarget = document.querySelector(".main__section");
+      var elementTarget = document.querySelector('.main__section');
       if (
         window.scrollY >
         elementTarget.offsetTop + elementTarget.offsetHeight
       ) {
-        document.querySelector(".scroll__header.alt").style.display = "block";
+        document.querySelector('.scroll__header.alt').style.display = 'block';
       } else {
-        document.querySelector(".scroll__header.alt").style.display = "none";
+        document.querySelector('.scroll__header.alt').style.display = 'none';
       }
     }, 100);
   }
